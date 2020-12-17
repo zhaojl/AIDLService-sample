667091c68b295e84e31679d3563bb068f60a8940
Jianling Zhao <zhaojianling@yinxiang.com>
2020-12-17 15:54:39 +0800

    remove jni

--- jni/src/main/jni/JNIHello.cpp
+++
@@ -1,5 +0,0 @@
-#include "com_java_jni_JNIUtils.h"
-JNIEXPORT jstring JNICALL Java_com_java_jni_JNIUtils_sayHelloFromJNI
-        (JNIEnv *env, jclass jclass){
-return env->NewStringUTF("Hello World From JNI!!!!!");
-}
